b2ce08941a3537f992c9dbf4c33fb8ae17486df6
happycodingpy <hugo.orlando2030@gmail.com>
2021-03-02 16:59:00 -0300

    sum array

---
+++ courseit/05-sum-array/script.js
@@ -0,0 +1,12 @@
+// Suma de array
+// Nos gustaría crear una función que retorne la suma de todos los números de un array.
+// ¿Podrías hacerlo?
+// [1, 2, 3], [-5, -5], [-3, 3]
+const arr = [1, 2, 3];
+const arraySum = arr => {
+  const result = (a, c) => a + c;
+  return arr.reduce(result);
+};
+
+const a = arraySum(arr);
+console.log(a);
