19429acf636427b0f127e64882784c998671ac35
happycodingpy <hugo.orlando2030@gmail.com>
2021-03-01 22:09:45 -0300

    suma de array

--- courseit/04-numOrStr/script.js
+++ courseit/04-numOrStr/script.js
@@ -1,3 +1,11 @@
 // ¿Es String o Number?
 // Determinar si el valor recibido (value) es 'number' o 'string'
-const numOrStr = value => {};
+// 'Hola', 5
+const value = 'Hola';
+const numOrStr = value => {
+  const r = typeof value === 'number' ? 'number' : 'string';
+  return r;
+};
+
+const a = numOrStr(value);
+console.log(a);
