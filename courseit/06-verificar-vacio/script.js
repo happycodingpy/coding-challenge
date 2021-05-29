7de229d0f7d10366c5001930b3183a66c8e13a58
happycodingpy <hugo.orlando2030@gmail.com>
2021-05-29 17:00:38 -0300

    verificar str vacio

---
+++ courseit/06-verificar-vacio/script.js
@@ -0,0 +1,7 @@
+// Verificar vacío
+// Queremos que la función isEmpty devuelva true si el string está vacío, y false si no lo está
+const str = '';
+const isEmpty = str => (str === '' ? true : false);
+
+const a = isEmpty(str);
+console.log(a);
