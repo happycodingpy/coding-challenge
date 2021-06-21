3380ae4627712f8f129493fa0410d1a4122a701a
happycodingpy <hugo.orlando2030@gmail.com>
2021-06-21 13:30:05 -0300

    Reversed strings

---
+++ coderwars/Fundamentals/04-Reversed-strings/script.js
@@ -0,0 +1,5 @@
+const solution = str => str.toString().split('').reverse().join('');
+console.log(solution('world'));
+console.log(solution('hello'));
+console.log(solution('h'));
+console.log(solution(''));
