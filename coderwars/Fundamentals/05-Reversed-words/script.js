60456e5dfd3b1a90d88b250c7ff24ff9cd45bde0
happycodingpy <hugo.orlando2030@gmail.com>
2021-06-21 13:30:18 -0300

    Reversed words

---
+++ coderwars/Fundamentals/05-Reversed-words/script.js
@@ -0,0 +1,7 @@
+const reverseWords = str => str.split(' ').reverse().join(' ');
+
+console.log(reverseWords('hello world!'));
+console.log(reverseWords('yoda doesnt speak like this'));
+console.log(reverseWords('foobar'));
+console.log(reverseWords('kata editor'));
+console.log(reverseWords('row row row your boat'));
